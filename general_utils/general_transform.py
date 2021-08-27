b02185da619fec3c108a935046798fdf19900e6c
stin911 <romitaalessio@gmail.com>
2021-08-27 15:43:28 +0200

    alternative transformation

---
+++ general_utils/general_transform.py
@@ -0,0 +1,29 @@
+import torchvision.transforms.functional as F
+import numpy as np
+import torch
+import PIL.Image as Image
+from torchvision import transforms as T
+
+
+class ResizeMine:
+    def __call__(self, sample):
+        image = F.resize(sample['image'], [512, 512], interpolation=T.InterpolationMode.BILINEAR)
+        mask = F.resize(sample['mask'], [512, 512], interpolation=T.InterpolationMode.NEAREST)
+        sample = {'image': image, 'mask': mask}
+        return sample
+
+
+class CToTensor:
+    def __call__(self, sample):
+        image = np.array(sample['image'])
+        mask = np.array(sample['mask'], np.float32)
+        image = image.astype('float32')
+        # normalize to the range 0-1
+        image /= 255.0
+
+        image = torch.from_numpy(image).float()
+        mask = torch.from_numpy(mask).float()
+        image = image.permute(2, 0, 1)
+
+        sample = {'image': image, 'mask': mask}
+        return sample
